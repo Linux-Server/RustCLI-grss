6dd20b0a180ce21cacf7e346fb1024a0a7e5ad30
Sachin <sachin6624@gmail.com>
2023-10-02 17:57:35 +0530

    cli app

--- grss/src/main.rs
+++ grss/src/main.rs
@@ -1,10 +1,6 @@
 #![allow(unused)]
 use clap::Parser;
-
-fn main() {
-    let args = Cli::parse();
-    println!("Hello, world!");
-}
+use std::fs;
 
 
 #[derive(Parser)]
@@ -12,3 +8,24 @@ struct Cli{
     pattern: String,
     path: std::path::PathBuf,
 }
+
+fn main() {
+    let args = Cli::parse();
+    let data = fs::read_to_string("hello.txt");
+
+    match data {
+        Ok(content) => println!("{:?}", content),
+        Err(err) => println!("{:?}", err)
+        
+    }
+
+    // for line in data.lines(){
+    //     if line.contains(&args.pattern){
+    //         println!("{:?}", line);
+    //     }
+        
+    // }
+  
+}
+
+
