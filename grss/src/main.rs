3c2f0f649d22ec4c75d5cb9b22a0c4dfd8cdc915
Sachin <sachin6624@gmail.com>
2023-10-02 18:26:35 +0530

    Box pointer error wrapping with into()

--- grss/src/main.rs
+++ grss/src/main.rs
@@ -1,6 +1,7 @@
 #![allow(unused)]
 use clap::Parser;
 use std::fs;
+use std::io;
 
 
 #[derive(Parser)]
@@ -9,22 +10,24 @@ struct Cli{
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main()-> Result<(), Box<dyn std::error::Error> >{
     let args = Cli::parse();
-    let data = fs::read_to_string("hello.txt");
+    let data = fs::read_to_string("hello1.txt");
 
-    match data {
-        Ok(content) => println!("{:?}", content),
-        Err(err) => println!("{:?}", err)
+    let result = match data {
+        Ok(content) =>content,
+        Err(err) => return Err(err.into())
         
     }
+    Ok(())
 
-    for line in data.lines(){
-        if line.contains(&args.pattern){
-            println!("{:?}", line);
-        }
+    // for line in data.lines(){
+    //     if line.contains(&args.pattern){
+    //         println!("{:?}", line);
+    //     }
         
-    }
+    // }
+    
   
 }
 
